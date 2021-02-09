64aa06f2989a27fad0e8c4c8861d993fb590b0c2
Max <maxib2007@gmail.com>
2021-02-09 12:02:07 -0300

    filled the options quiz based in list quiz model

--- app/src/main/java/com/maxwell/quiz/Question.kt
+++ app/src/main/java/com/maxwell/quiz/Question.kt
@@ -2,7 +2,7 @@ package com.maxwell.quiz
 
 data class Question(
     val id: Int,
-    val question: String,
+    val text: String,
     val image: Int,
     val optionOne: String,
     val optionTwo: String,
--- app/src/main/java/com/maxwell/quiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/maxwell/quiz/QuizQuestionsActivity.kt
@@ -2,6 +2,9 @@ package com.maxwell.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,32 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_questions)
 
         val questionList = Constants.getQuestions()
+
+        val currentPosition = 1
+        val question: Question = questionList[currentPosition - 1]
+
+        val pb = findViewById<ProgressBar>(R.id.pb)
+        pb.progress = currentPosition
+
+        val tvProgress = findViewById<TextView>(R.id.tvProgress)
+        tvProgress.text = "$currentPosition/${pb.max}"
+
+        val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
+        tvQuestion.text = question.text
+
+        val ivFlag = findViewById<ImageView>(R.id.ivFlag)
+        ivFlag.setImageResource(question.image)
+
+        val tvOptionOne = findViewById<TextView>(R.id.tvOptionOne)
+        tvOptionOne.text = question.optionOne
+
+        val tvOptionTwo = findViewById<TextView>(R.id.tvOptionTwo)
+        tvOptionTwo.text = question.optionTwo
+
+        val tvOptionThree = findViewById<TextView>(R.id.tvOptionThree)
+        tvOptionThree.text = question.optionThree
+
+        val tvOptionFour = findViewById<TextView>(R.id.tvOptionFour)
+        tvOptionFour.text = question.optionFour
     }
 }
