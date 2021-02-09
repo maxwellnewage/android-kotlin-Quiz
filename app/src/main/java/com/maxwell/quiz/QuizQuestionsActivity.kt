737404fdeb96d567aaf4037e2117d129c7ac7bc9
Max <maxib2007@gmail.com>
2021-02-09 12:43:38 -0300

    implemented view logic to select an option for quiz

--- app/src/main/java/com/maxwell/quiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/maxwell/quiz/QuizQuestionsActivity.kt
@@ -1,43 +1,106 @@
 package com.maxwell.quiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private var mCurrentPosition: Int = 1
+    private lateinit var mQuestionsList: ArrayList<Question>
+    private var mSelectedOptionPosition: Int = 0
+    private lateinit var pb: ProgressBar
+    private lateinit var tvProgress: TextView
+    private lateinit var tvQuestion: TextView
+    private lateinit var ivFlag: ImageView
+    private lateinit var tvOptionOne: TextView
+    private lateinit var tvOptionTwo: TextView
+    private lateinit var tvOptionThree: TextView
+    private lateinit var tvOptionFour: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionList = Constants.getQuestions()
+        initViews()
 
-        val currentPosition = 1
-        val question: Question = questionList[currentPosition - 1]
+        setClickEvents()
 
-        val pb = findViewById<ProgressBar>(R.id.pb)
-        pb.progress = currentPosition
+        setQuestion()
+    }
 
-        val tvProgress = findViewById<TextView>(R.id.tvProgress)
-        tvProgress.text = "$currentPosition/${pb.max}"
+    private fun initViews() {
+        pb = findViewById<ProgressBar>(R.id.pb)
+        tvProgress = findViewById(R.id.tvProgress)
+        tvQuestion = findViewById<TextView>(R.id.tvQuestion)
+        ivFlag = findViewById<ImageView>(R.id.ivFlag)
+        tvOptionOne = findViewById<TextView>(R.id.tvOptionOne)
+        tvOptionTwo = findViewById<TextView>(R.id.tvOptionTwo)
+        tvOptionThree = findViewById<TextView>(R.id.tvOptionThree)
+        tvOptionFour = findViewById<TextView>(R.id.tvOptionFour)
+    }
 
-        val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
+    private fun setQuestion() {
+        mQuestionsList = Constants.getQuestions()
+
+        mCurrentPosition = 1
+        val question = mQuestionsList[mCurrentPosition - 1]
+
+        defaultOptionsView()
+
+        pb.progress = mCurrentPosition
+
+        tvProgress.text = "$mCurrentPosition/${pb.max}"
+
         tvQuestion.text = question.text
 
-        val ivFlag = findViewById<ImageView>(R.id.ivFlag)
         ivFlag.setImageResource(question.image)
 
-        val tvOptionOne = findViewById<TextView>(R.id.tvOptionOne)
         tvOptionOne.text = question.optionOne
-
-        val tvOptionTwo = findViewById<TextView>(R.id.tvOptionTwo)
         tvOptionTwo.text = question.optionTwo
-
-        val tvOptionThree = findViewById<TextView>(R.id.tvOptionThree)
         tvOptionThree.text = question.optionThree
-
-        val tvOptionFour = findViewById<TextView>(R.id.tvOptionFour)
         tvOptionFour.text = question.optionFour
+    }
+
+    private fun setClickEvents() {
+        tvOptionOne.setOnClickListener(this)
+        tvOptionTwo.setOnClickListener(this)
+        tvOptionThree.setOnClickListener(this)
+        tvOptionFour.setOnClickListener(this)
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptNum
+
+        tv.typeface = Typeface.DEFAULT_BOLD
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(tvOptionOne)
+        options.add(tvOptionTwo)
+        options.add(tvOptionThree)
+        options.add(tvOptionFour)
+
+        for (op in options) {
+            op.typeface = Typeface.DEFAULT
+            op.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.tvOptionOne -> selectedOptionView(tvOptionOne, 1)
+            R.id.tvOptionTwo -> selectedOptionView(tvOptionTwo, 2)
+            R.id.tvOptionThree -> selectedOptionView(tvOptionThree, 3)
+            R.id.tvOptionFour -> selectedOptionView(tvOptionFour, 4)
+        }
     }
 }
