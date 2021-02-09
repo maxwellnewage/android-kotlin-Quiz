69f0784634120327d3ceee61b7e28e814c891e9a
Max <maxib2007@gmail.com>
2021-02-09 11:15:00 -0300

    connected main activity with quiz

--- app/src/main/java/com/maxwell/quiz/MainActivity.kt
+++ app/src/main/java/com/maxwell/quiz/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.maxwell.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btStart = findViewById<Button>(R.id.btStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btStart.setOnClickListener {
+            if(etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
